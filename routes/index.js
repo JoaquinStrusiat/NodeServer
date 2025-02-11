8ba892fbb4618db91195c57175b95d50a078a633
Joaquin Strusiat <joaquinpjs@gmail.com>
2025-02-11 19:44:22 -0300

    11/02 - 19:43 - New log in router

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,24 @@ const express = require('express');
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
+const os = require('os');
+
+function getLocalIP() {
+    const interfaces = os.networkInterfaces();
+    for (const interfaceName in interfaces) {
+        const interfaceInfo = interfaces[interfaceName];
+        for (const iface of interfaceInfo) {
+            // Ignora direcciones IPv6 y direcciones que no sean internas
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return '0.0.0.0'; // Si no se encuentra ninguna dirección IP
+}
+
+const localIP = getLocalIP();
+
 
 function removeExtension(fileName) {
     return fileName.split(".").shift();
@@ -23,7 +41,7 @@ fs.readdirSync(__dirname).forEach((version) => {
                 //creo la ruta
                 router.use(`/${version}/${nameCleaned}`, require(filePath));
                 //Imprimo las rutas
-                console.log(`Ruta creada: http://localhost:3000/${version}/${nameCleaned}`);
+                console.log(`Ruta creada: http://${localIP}:${process.env.PORT || 3001}/${version}/${nameCleaned}`);
             }
         });
     }
